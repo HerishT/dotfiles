70b2451ffef99ea42b89cfade4314184ae7cdd77
Herish Tripathi <herish.tripathi@princeton.edu>
2025-07-09 00:12:36 -0400

    Initial commit

---
+++ private_Library/Application Support/Ubersicht/widgets/simple-bar/lib/styles/pywal/pywal.js
@@ -0,0 +1,16 @@
+export const variables = {
+    "cursor": "#c3c4c4",
+    "number": "#c3c4c4",
+    "parens": "#646D6F",
+    "result": "#5E6464",
+    "comment": "#5a6e6e",
+    "matched": "#5E6464",
+    "function": "#51534D",
+    "operator": "#696150",
+    "variable": "#5C625C",
+    "scrollbar": "#696150",
+    "separator": "#696150",
+    "background": "#121313",
+    "editorbackground": "#121313"
+}
+;
